6806d596dfedc97d922dc2b0cae5beeb3f31f05a
MRmdum <107920254+MRmdum@users.noreply.github.com>
2023-01-05 00:19:14 +0100

    Add files via upload

---
+++ Quickresponse.py
@@ -0,0 +1,70 @@
+import requests
+import base64
+from bs4 import BeautifulSoup
+from PIL import Image
+
+from pyzbar.pyzbar import decode
+import cv2
+
+mySession = requests.Session()
+
+##connection to web page
+
+url = "http://challenge01.root-me.org/programmation/ch7/"
+page = mySession.get(url)
+
+#parsing
+soup = BeautifulSoup(page.content,"html.parser")
+results = soup.find_all('img')[0].attrs['src'].split(",")[1]
+
+#decoding base 64 + download
+with open("imageToSave.png", "wb") as fh:
+    fh.write(base64.decodebytes(results.encode('utf-8')))
+
+
+#adding squares in qr's corners#
+cropingValue = (18,18,282,282)
+
+im = Image.open('imageToSave.png')
+im.crop(cropingValue).save('imageToSave.png')
+im = Image.open('imageToSave.png')
+
+size = (29,29)
+im = im.resize(size)
+im.save('imageToSave.png')
+
+pixelMap = im.load()
+
+img = Image.new( im.mode, im.size)
+pixelsNew = img.load()
+for i in range(img.size[0]):
+    for j in range(img.size[1]):
+        if i<8 and j<8:
+            print("ahhhh")
+            pixelsNew[i,j] = (0,0,0)
+        if pixelMap[i,j][0] >= 100 or pixelMap[i,j][1] >= 100 or pixelMap[i,j][2] >= 100: 
+            pixelsNew[i,j] = (255,255,255)
+
+img.save('imageToSave.png')
+###
+
+# decode()
+
+# text = text.replace('\n', '').replace(' ', '').replace(',', '').replace('\'', '')
+
+# print(text)
+###
+text = 'aie'
+
+post_params = {'metu' : text}
+headers = {'Content-Type' : 'application/x-www-form-urlencoded'}
+cookies ={'PHPSESSID': mySession.cookies.get('PHPSESSID')}
+
+rcontent = mySession.post(url,data=post_params,cookies =cookies)
+
+soup = BeautifulSoup(rcontent.content,"html.parser")
+results = str(soup.find_all('p')[0]).removeprefix("<p>").removesuffix(".<br/></p>")
+
+print(results)
+
+#flag: dtePZJgVAfaU
